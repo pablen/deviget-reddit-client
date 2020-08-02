504217b218f40cf38cce5286093ca27f208e6789
Pablo Enrici <pabloenrici@gmail.com>
2020-08-02 13:50:04 -0300

    Add basic responsive mobile-first styling for layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 
+import styles from './App.module.css'
 import posts from '../top.json'
 
 function App() {
   const selectedPost = posts.data.children[0]
 
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false)
+
+  const handleSidebarToggle = useCallback(() => {
+    setIsSidebarVisible((s) => !s)
+  }, [])
+
+  // Reset sidebar toggle when resizing viewport
+  useEffect(() => {
+    if (!window.matchMedia) return
+    const mql = window.matchMedia('(max-width: 800px)')
+    const handler = (ev: MediaQueryListEvent) => {
+      if (ev.matches) setIsSidebarVisible(false)
+    }
+    mql.addEventListener('change', handler)
+    return () => mql.removeEventListener('change', handler)
+  }, [])
+
   return (
-    <div>
-      <section>
-        <h1>Reddit Posts</h1>
-        <ul>
+    <div className={styles.container}>
+      <button
+        type="button"
+        className={styles.sidebarToggle}
+        onClick={handleSidebarToggle}
+      >
+        {isSidebarVisible ? 'Hide Sidebar' : 'Show Sidebar'}
+      </button>
+      <nav
+        className={`${styles.sidebar} ${
+          isSidebarVisible ? styles.isSidebarVisible : styles.isSidebarHidden
+        }`}
+      >
+        <h1 className={styles.sidebarTitle}>Reddit Posts</h1>
+        <ul className={styles.postsList}>
           {posts.data.children.map((post) => (
             <li key={post.data.id}>
-              <button type="button">
+              <button
+                aria-current={post.data.id === selectedPost.data.id}
+                className={styles.postsListItem}
+                type="button"
+              >
                 <p>Author: {post.data.author}</p>
-                <p>Created: {post.data.created}</p>
+                <p>
+                  Created: <time>{post.data.created}</time>
+                </p>
                 {post.data.thumbnail && (
                   <img src={post.data.thumbnail} alt="" />
                 )}
@@ -29,22 +64,28 @@ function App() {
             </li>
           ))}
         </ul>
-        <button type="button">Dismiss All</button>
-      </section>
-      <main>
-        <h1>{selectedPost.data.author}</h1>
-        <p>Created: {selectedPost.data.created}</p>
-        {selectedPost.data.thumbnail && (
-          <img src={selectedPost.data.thumbnail} alt="" />
-        )}
-        <p>{selectedPost.data.title}</p>
-        <a
-          target="_blank"
-          href={selectedPost.data.url}
-          rel="noreferrer noopener"
-        >
-          {selectedPost.data.url}
-        </a>
+        <button className={styles.dismissAllBtn} type="button">
+          Dismiss All
+        </button>
+      </nav>
+      <main className={styles.main}>
+        <article>
+          <h1>{selectedPost.data.author}</h1>
+          <p>
+            Created: <time>{selectedPost.data.created}</time>
+          </p>
+          {selectedPost.data.thumbnail && (
+            <img src={selectedPost.data.thumbnail} alt="" />
+          )}
+          <p>{selectedPost.data.title}</p>
+          <a
+            target="_blank"
+            href={selectedPost.data.url}
+            rel="noreferrer noopener"
+          >
+            {selectedPost.data.url}
+          </a>
+        </article>
       </main>
     </div>
   )
